c794ac10233535475978896fe0fb72a3dff1556b
Huynh Thanh Dat <huynhthanhdat1108@gmail.com>
2023-05-19 02:10:08 +0700

    change code rotate video

--- node-api-gateway/internal/server/service/camera_rotation_service.js
+++ node-api-gateway/internal/server/service/camera_rotation_service.js
@@ -1,99 +1,147 @@
-
 const Cam = require("onvif").Cam;
 
 class CameraRotationService {
+  getConnection(cameraConfig) {
+    return new Promise((resolve, reject) => {
+      new Cam(cameraConfig, function (err) {
+        if (err) {
+          reject(err);
+        }
+        resolve(this);
+      });
+    });
+  }
 
-    getConnection(cameraConfig) {
-        return new Promise((resolve, reject) => {
-            new Cam(cameraConfig, function (err) {
-                if (err) {
-                    reject(err);
-                }
-                resolve(this);
-            });
+  async relativeMove(connection, x, y, z, zoom) {
+    return new Promise((resolve, reject) => {
+      connection.relativeMove({
+          x: x,
+          y: y,
+          zoom: zoom,
+        }, (err) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(this);
+          // connection.getStatus({}, (err, object)=>{
+          //   if (err) {
+          //     reject(err);
+          //   }
+          //   resolve(object);
+          // })
         });
-    }
+    });
+  }
 
-    async relativeMove(cameraConfig, x, y, z, zoom) {
-        const connection = await this.getConnection(cameraConfig);
-        connection.relativeMove({
+  async absoluteMove(cameraConfig, x, y, z, zoom) {
+    const connection = await this.getConnection(cameraConfig);
+    return new Promise((resolve, reject) => {
+      connection
+        .absoluteMove({
+          x: x,
+          y: y,
+          zoom: zoom,
+        }, (err) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(this);
+        });
+    });
+  }
+
+  async continuousMove(cameraConfig, x, y, z, zoom) {
+    const connection = await this.getConnection(cameraConfig);
+    return new Promise((resolve, reject) => {
+        connection
+          .continuousMove({
             x: x,
             y: y,
-            zoom: zoom
-        });
-    }
-
-    async absoluteMove(cameraConfig, x, y, z, zoom) {
-        const connection = await this.getConnection(cameraConfig);
-        connection.absoluteMove({
-            x: x,
-            y: y,
-            zoom: zoom
-        });
-    }
-
-    async continuousMove(cameraConfig, x, y, z, zoom) {
-        const connection = await this.getConnection(cameraConfig);
-        connection.continuousMove({
-            x: x,
-            y: y,
-            zoom: zoom
-        });
-    }
-
-    async stop(cameraConfig) {
-        const connection = await this.getConnection(cameraConfig);
-        connection.stop();
-    }
-
-    async getStatus(cameraConfig) {
-        const connection = await this.getConnection(cameraConfig);
-        return new Promise((resolve, reject) => {
-            connection.getStatus((err, object) => {
-                if (err) {
-                    reject(err);
-                }
-                resolve(object);
-            });
-        })
-    }
-
-
-    async getPresets(cameraConfig) {
-        const connection = await this.getConnection(cameraConfig);
-        return new Promise((resolve, reject) => {
-            connection.getPresets((err, object) => {
-                if (err) {
-                    reject(err);
-                }
-                resolve(object);
-            });
-        })
-    }
-
-    async gotoPreset(cameraConfig, preset, profileToken) {
-        const connection = await this.getConnection(cameraConfig);
-        connection.gotoPreset(
-            {
-                preset: preset
+            zoom: zoom,
+          }, (err) => {
+            if (err) {
+              reject(err);
             }
-        );
-    }
+            resolve(this);
+          });
+    });
+  }
 
+  async stop(cameraConfig) {
+    const connection = await this.getConnection(cameraConfig);
+    return new Promise((resolve, reject) => {
+        connection.stop({}, (err) => {
+            if (err) {
+              reject(err);
+            }
+            resolve(this);
+        });
+    });
+  }
 
-    async setHomePosition(cameraConfig, options) {
-        const connection = await this.getConnection(cameraConfig);
-        connection.setHomePosition(options);
-    }
+  async getStatus(cameraConfig, options) {
+    const connection = await this.getConnection(cameraConfig);
+    return new Promise((resolve, reject) => {
+      console.log(options)
+      connection.getStatus(options, (err, object)=>{
+          if (err) {
+              reject(err);
+          }
+          resolve(object);
+      });
+  })
+  }
 
-    async gotoHomePosition(cameraConfig) {
-        const connection = await this.getConnection(cameraConfig);
-        connection.gotoHomePosition({}, (err) => {});
-    }
+  async getPresets(cameraConfig) {
+    const connection = await this.getConnection(cameraConfig);
+    return new Promise((resolve, reject) => {
+      connection.getPresets({}, (err, object) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(object);
+      });
+    });
+  }
+
+  async gotoPreset(cameraConfig, preset, profileToken) {
+    const connection = await this.getConnection(cameraConfig);
+    connection.gotoPreset({
+      preset: preset,
+    });
+  }
+
+  async setHomePosition(cameraConfig, options) {
+    const connection = await this.getConnection(cameraConfig);
+    return new Promise((resolve, reject) => {
+        connection.setHomePosition(options, (err)=> {
+            if (err) {
+                reject(err);
+              }
+              resolve(this);
+        });
+    })
+  }
+
+  async gotoHomePosition(connection, options) {
+    return new Promise((resolve, reject) => {
+        connection.gotoHomePosition(options, (err)=> {
+              if (err) {
+                  reject(err);
+                }
+                connection.getStatus({}, (err, object)=>{
+            if (err) {
+              reject(err);
+            }
+            resolve(object);
+          })
+          });
+      })
+  }
 }
 
 function newCameraRotationService() {
-    return new CameraRotationService();
+  return new CameraRotationService();
 }
 
 module.exports.newCameraRotationService = newCameraRotationService;
